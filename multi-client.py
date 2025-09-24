9d2ad4a71a59950b25b298bd3b458f0dc96aa4c8
Bernard Leung <bernardleung1707@gmail.com>
2025-09-25 01:38:36 +1000

    adding multiple features

--- multi-client.py
+++ multi-client.py
@@ -1,13 +1,19 @@
 import sys
 import os
+from prompt_toolkit import *
+from prompt_toolkit.history import *
+from prompt_toolkit.patch_stdout import patch_stdout
 from socket import *
 from threading import Thread
+from datetime import datetime
 
 class Client:
     def __init__(self, HOST, PORT):
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.connect((HOST,PORT))
-        self.name = input("Enter your name: ")
+        self.name = self.get_name()
+        self.hist = InMemoryHistory()
+        self.session = PromptSession()
 
         self.talk()
 
@@ -17,59 +23,118 @@ class Client:
         self.send_message()
 
     def send_message(self):
+        usr_input: str
+        
         while True:
-            usr_input = input("You: ")
+            with patch_stdout():
+                usr_input = self.session.prompt("You: ")
+
+            if not usr_input.strip():
+                continue
+
+            time = f"[{datetime.now().strftime('%H:%M')}]"
 
             if usr_input.strip()[0] == "/":
+                print(f"\033[AYou: {usr_input} {time}")
                 self.command(usr_input)
             else:
-                message = self.name + ": " + usr_input
+                message = f"{self.name}: {usr_input} {time}"
+                self.hist.append_string(message)
                 self.socket.send(message.encode())
+                print(f"\033[AYou: {usr_input} {time}")
 
-    def command(self, input):
+    def command(self, input: str):
         """
             /msg username message
             /msgOpen username
             /msgClose username
-            /users
             /name newname
-            timestamps
             /help
-            emojis or text formatting
-            connection limits
         """
 
-        split = input.split('/')
+        split = input.split()
         command = split[0]
 
-        # if len(split) == 3:
-            
-        # elif len(split) == 2:
-        #     if command == "msgOpen":
-
-        #     elif command == "msgClose":
-
-        #     elif command == "name":
-        # else:
-        #     if command == "leave":
-        #         self.socket.send(input.encode())
-        #         self.socket.close()
-        #         sys.exit(0)
-        #     elif command == "help":
-
-        #     elif command == "users":
+        match command:
+            case "/msg":
+                self.msg(input)
+            case "/msgOpen":
+                self.msgOpen(input)
+            case "/msgClose":
+                self.msgClose(input)
+            case "/name":
+                self.change_name(input)
+            case "/leave":
+                self.socket.send(input.encode())
+                self.socket.close()
+                print_formatted_text(ANSI("\033[31mYou have left"))
+                os._exit(0)
+            case "/help":
+                self.help(input)
+            case "/users":
+                self.socket.send(input.encode())
+            case "/history":
+                self.history()
+            case "/clear":
+                os.system("clear")
+            case _:
+                print_formatted_text(ANSI("\033[31mThis is not a valid command"))
 
     def receive_message(self):
         while True:
             message = self.socket.recv(1024).decode()
 
-            if not message.strip():
-                print("Server has now shutdown")
+            if message.split("///")[0] == "/users":
+                self.users(message)
+                continue
+            elif not message.strip():
+                print_formatted_text(ANSI("\033[31mServer has now shutdown"))
                 self.socket.close()
                 os._exit(0)
+            
+            print_formatted_text(ANSI(message))
+                
+    def get_name(self):
+        name = ""
+        
+        while not name.strip():
+            name = prompt("Enter your name: ")
 
-            print(f"\r{message}")
-            print("You: ", end="", flush=True)
+            if name == "You":
+                print_formatted_text(ANSI("\033[31mThis is not a valid name"))
+                name = ""
+
+        return name
+
+    # def change_name(self, input):
+    
+
+    # def msg(self, input):
+    # def msgOpen(self, input):
+    # def msgClose(self, input):
+    # def help(self, input):
+
+
+    def users(self, input: str):
+        users = input.split("///")[1:]
+
+        print_formatted_text(ANSI("\033[34mUSERS START: \n"))
+
+        for user in users:
+            print_formatted_text(ANSI(f"\033[34m{user}"))
+        
+        print_formatted_text(ANSI("\n \033[34mUSERS END \n"))
+
+    def history(self):
+        history = self.hist.get_strings()
+
+        print_formatted_text(ANSI("\033[34mHISTORY START: \n"))
+
+        for line in history:
+            print_formatted_text(ANSI(f"\033[34m{line}"))
+
+        print_formatted_text(ANSI("\n \033[34mHISTORY END \n"))
+
 
 PORT = int(sys.argv[1])
 
--- multi-server.py
+++ multi-server.py
@@ -3,6 +3,7 @@ import sys
 from socket import *
 from threading import Thread
 import time
+from datetime import datetime
 
 class Server:
     def __init__(self, HOST, PORT):
@@ -21,26 +22,36 @@ class Server:
             name = client_socket.recv(1024).decode()
             client = {"name": name, "socket": client_socket}
 
-            self.broadcast(client, name + " has joined the chat!")
-            client_socket.send(f"Welcome, {name}!".encode())
+            time = f"[{datetime.now().strftime('%H:%M')}]"
+            self.broadcast(client, f"\033[31m{name} has joined the chat! [{time}]")
+            client_socket.send(f"Welcome, {name}! [{time}]".encode())
 
             self.clients.append(client)
             Thread(target=self.handle_new_client, args=(client, addr), daemon=True).start()
 
     def server_close(self):
         while True:
-            if input("") == "/exit":
-                print("Server shutdown in 10 seconds")
-                self.broadcast_all("Server will be shutting down in 10 seconds")
-                time.sleep(10)
+            inp = input("")
+            split = inp.split()
 
-                for client in self.clients[:]:
-                    client["socket"].close()
-                    self.clients.remove(client)
+            if len(split) == 2 and split[0] == "/exit":
+                self.shutdown(int(split[1]))
+            elif input("") == "/exit":
+                self.shutdown(5)
 
-                self.socket.shutdown(SHUT_RDWR)
-                self.socket.close()
-                os._exit(0)
+    def shutdown(self, shut_time):
+        curr_time = f" [{datetime.now().strftime('%H:%M')}]"
+        print(f"Server shutdown in {shut_time} seconds")
+        self.broadcast_all(f"\033[31mServer will be shutting down in {shut_time} seconds [{curr_time}]")
+        time.sleep(shut_time)
+
+        for client in self.clients[:]:
+            client["socket"].close()
+            self.clients.remove(client)
+
+        self.socket.shutdown(SHUT_RDWR)
+        self.socket.close()
+        os._exit(0)
 
     def broadcast_all(self, message):
         disconnected = []
@@ -56,24 +67,29 @@ class Server:
             
     def handle_new_client(self, client, addr):
         name = client["name"]
-        socket = client["socket"]
+        sock: socket = client["socket"]
 
         while True:
             try:
-                message = socket.recv(1024).decode()
+                message = sock.recv(1024).decode()
+                time = f"[{datetime.now().strftime('%H:%M')}]"
                 if message.strip() == "/leave":
-                    self.broadcast(client, name + " has left the chat!")
+                    self.broadcast(client, f"\033[31m{name} has left the chat! [{time}]")
                     print("Disconnection from: " + str(addr))
                     self.clients.remove(client)
-                    socket.close()
+                    sock.close()
                     break
+                elif message.strip() == "/users":
+                    names = [c["name"] for c in self.clients]
+                    users = "///".join(names)
+                    sock.send(f"/users///{users}".encode()) 
                 else:
                     self.broadcast(client, message)
             except Exception:
-                self.broadcast(client, name + " has left the chat!")
+                self.broadcast(client, f"\033[31m{name} has left the chat! [{time}]")
                 if client in self.clients:
                     self.clients.remove(client)
-                socket.close()
+                sock.close()
                 break
 
     def broadcast(self, sender, message):
@@ -81,7 +97,8 @@ class Server:
         for client in self.clients[:]:  
             if client["name"] != sender["name"]:
                 try:
-                    client["socket"].send(message.encode())
+                    sock : socket = client["socket"]
+                    sock.send(message.encode())
                 except Exception:
                     disconnected.append(client)
 
@@ -92,5 +109,5 @@ class Server:
 PORT = int(sys.argv[1])
 
 if __name__ == "__main__":
-    server = Server("0.0.0.0", PORT)
+    server = Server("localhost", PORT)
     server.listen()
